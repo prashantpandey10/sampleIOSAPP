f1fcc65f48380bdd1391eff36c5b4fcecfe44a11
Prashant Pandey <prashantpandey@prashants-macbook-pro.local>
2015-10-02 14:34:49 +0530

    Initial Commit

---
+++ iBearded Robot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBearded Robot
+//
+//  Created by Prashant Pandey on 10/2/15.
+//  Copyright © 2015 Prashant Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iBearded RobotTests/iBearded_RobotTests.swift
@@ -0,0 +1,36 @@
+//
+//  iBearded_RobotTests.swift
+//  iBearded RobotTests
+//
+//  Created by Prashant Pandey on 10/2/15.
+//  Copyright © 2015 Prashant Pandey. All rights reserved.
+//
+
+import XCTest
+@testable import iBearded_Robot
+
+class iBearded_RobotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
